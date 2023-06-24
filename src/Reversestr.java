1c647aee51faa93653cd642a0d2c625ede0bd3a3
monish <monishanand.j@gmail.com>
2023-06-24 22:30:43 +0530

    5

---
+++ src/Reversestr.java
@@ -0,0 +1,8 @@
+public class Reversestr {
+    public static void main(String[] args) {
+        String a="monish";
+        for (int i=a.length()-1;i>=0;i--){
+            System.out.print(a.charAt(i));
+        }
+    }
+}
